34689931bfbd286bdb54600e3471c48e462c84aa
taiga_labs_mikle <Mikhail.Yakushkin@optimum-is.com>
2026-03-25 12:32:22 +0300

    add more logs

--- tgbot/dialogs/menu/main/callbacks.py
+++ tgbot/dialogs/menu/main/callbacks.py
@@ -1,3 +1,5 @@
+import logging
+
 from aiogram.types import CallbackQuery
 from aiogram_dialog import DialogManager
 from aiogram_dialog.widgets.kbd import Button
@@ -5,12 +7,25 @@ from aiogram_dialog.widgets.kbd import Button
 from dialogs.menu.technical_support.states import TechnicalSupport
 from dialogs.menu.profile.states import Profile
 
+logger = logging.getLogger(__name__)
+
 
 async def click_btn_technical_support(
     callback: CallbackQuery, button: Button, manager: DialogManager
 ):
+    logger.info(
+        "click_btn_technical_support: user_id=%s chat_id=%s callback_data=%r",
+        callback.from_user.id,
+        callback.message.chat.id if callback.message else None,
+        callback.data,
+    )
     await callback.answer()
     await manager.start(state=TechnicalSupport.select_support)
+    logger.info(
+        "click_btn_technical_support: started state=%s for user_id=%s",
+        TechnicalSupport.select_support.state,
+        callback.from_user.id,
+    )
 
 
 async def click_btn_profile(
